fc8f76412d3aeaa293d58140cc613692aab0d4c8
honey <honeychintal@gmail.com>
2021-03-31 20:38:47 +0530

    First commit, added REST API to access items, stores, and user authentication.

---
+++ app.py
@@ -0,0 +1,35 @@
+# from typing_extensions import Required
+from typing import Mapping
+from security import authenticate, identity
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "rama"
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity) # jwt creates /auth
+
+
+api.add_resource(Store,'/store/<string:name>')
+api.add_resource(Item, '/Item/<string:name>')
+api.add_resource(ItemList,'/Items/')
+api.add_resource(StoreList,'/stores')
+api.add_resource(UserRegister,'/register')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
